5acb0ef0b6a774c6876211a7b4774a2d510205fc
toscy <123191534+Lajous-Harold@users.noreply.github.com>
2024-12-10 15:23:31 +0100

    Changement vers POO PHP

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # e-commerce
- Entrainement au developpement web par un cas concret
+
+Entrainement au developpement web par un cas concret
+
+## Installation
+
+If you don't have Composer installed, you can install it by typing this command :
+
+```bash
+sudo apt-get install composer
+```
+
+Then, you can install the project dependencies by running this command :
+
+```bash
+composer install
+```
+
+## Start server using php
+
+```bash
+php -S localhost:5001 -t public
+```
+
+### You can now access the project at `http://localhost:5001` in your browser.
---
+++ src/View/Partial/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/020fe7311e.js" crossorigin="anonymous"></script>
+    <title>E-commerce training</title>
+  </head>
+  <body>
+    <section id="header">
+      <a href="#">
+        <img src="img/logo.png" class="logo" alt="" />
+      </a>
+
+      <div>
+        <ul id="navbar">
+          <li><a class="active" href="index.html">Home</a></li>
+          <li><a href="HTML/shop.html">Shop</a></li>
+          <li><a href="HTML/blog.html">Blog</a></li>
+          <li><a href="HTML/about.html">About</a></li>
+          <li><a href="HTML/contact.html">Contact</a></li>
+          <li id="lg-bag">
+            <a href="cart.html"><i class="fa-solid fa-basket-shopping"></i></a>
+          </li>
+          <i id="close" class="fa-solid fa-xmark"></i>
+        </ul>
+      </div>
+      <div id="mobile">
+        <a href="cart.html"><i class="fa-solid fa-basket-shopping"></i></a>
+        <i id="bar" class="fas fa-outdent"></i>
+      </div>
+    </section>
